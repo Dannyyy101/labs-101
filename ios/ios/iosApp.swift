4103c1247fec32868d3291b82633d53ff1f42c23
Daniel Stöcklein <danielfelixstoecklein@gmail.com>
2026-08-20 12:09:22 +0200

    add HealthStore example to ios app

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Daniel Stöcklein on 19.08.26.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MyView()
+        }
+    }
+}
